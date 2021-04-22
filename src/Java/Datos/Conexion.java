c4a285321821e56c514f9641f0cffab4d1c34881
P4k3n <P4k3n@protonmail.com>
2021-04-21 22:48:44 -0600

    l

--- src/Java/Datos/Conexion.java
+++ src/Java/Datos/Conexion.java
@@ -12,6 +12,57 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 
+
+
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+ 
+
+ 
+
+ 
+
+/*
+public class Conexion {
+
+ 
+
+ 
+
+ 
+
+    Connection getConexion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}*/
+public class Conexion {
+
+ 
+
+    public Connection getConnection() {
+        return con;
+    }
+
+ 
+
+    Connection con;
+   public Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/dbprueba?allowPublicKeyRetrieval=true&useSSL=false", "root", "");
+            
+        }catch(Exception e){
+            System.err.println("Error: " +e);
+        }
+    }
+}
+
+
+
 /*
 public class Conexion {
 
@@ -19,7 +70,7 @@ public class Conexion {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-}*/
+}
 public class Conexion {
     private static String DRIVER = "com.mysql.jdbc.Driver";
     private static String USER = "root";
@@ -35,16 +86,35 @@ public class Conexion {
             JOptionPane.showMessageDialog(null,"ERROR EN EL DRIVER"+e);
         }
     }
-    public Connection getConnection(){
-        
-        Connection con = null;
+    
+    
+    
+    
+    
+   Connection con;
+   public Conexion(){
         try{
-            con = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
-            JOptionPane.showMessageDialog(null,"Conexion exitosa");
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null,"ERROR en la conexion"+e);
+            Class.forName("com.mysql.jdbc.Driver");
+            con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/dbprueba?useSSL=false", "root", "");
+            
+        }catch(Exception e){
+            System.err.println("Error: " +e);
         }
-        return con;
     }
-
-}
+      public static void main(String[] args) {
+        Conexion cn = new Conexion();
+        Statement st;
+        ResultSet rs;
+        try {
+            st = cn.con.createStatement();
+            rs = st.executeQuery("select * from cursos");
+            while (rs.next()) {
+                System.out.println(rs.getInt("codigo") + " " + rs.getString("nombre")
+                        + " " + rs.getString("tematica") + " " + rs.getString("costo")
+                        + " " + rs.getString("status") + " " + rs.getString("logo"));
+            }
+            cn.con.close();
+        } catch (Exception e) {
+        }
+    }
+}*/
